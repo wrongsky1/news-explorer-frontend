19dbbb40db3c6d8d4e819d9bd617b81ef4bc023f
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-11-10 20:07:50 +0300

    add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,9 +11,39 @@ import Footer from '../Footer/Footer';
 
 
 function App() {
+    const history = useHistory();
+
+    const [activePage, setActivePage] = React.useState('main');
+    const [loggedIn, setLoggedIn] = React.useState(false);
+    const [mobileMenuOpened, setMobileMenuOpened] = React.useState(false);
+
+    function setMainPageActive() {
+        setActivePage('main');
+    }
+
+    function setSavedNewsPageActive() {
+        setActivePage('saved-news');
+    }
+
+    function toggleMobileMenu() {
+        setMobileMenuOpened(!mobileMenuOpened);
+    }
+
+    function handleLogout() {
+        setMainPageActive();
+        setLoggedIn(false);
+        history.push('/');
+    }
   return (
     <div className="page">
         <Header
+            activePage={activePage}
+            setMainPageActive={setMainPageActive}
+            setSavedNewsPageActive={setSavedNewsPageActive}
+            loggedIn={loggedIn}
+            mobileMenuOpened={mobileMenuOpened}
+            toggleMobileMenu={toggleMobileMenu}
+            handleLogout={handleLogout}
         />
         <Switch>
             <Route path="/" exact>
