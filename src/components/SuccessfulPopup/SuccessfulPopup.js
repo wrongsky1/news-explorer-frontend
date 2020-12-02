335121a37b9367f48ba1311c102a1fa98db0a123
Misha Razlivakhin <wrongsky1@yandex.ru>
2020-12-02 23:44:11 +0300

    refact

--- src/components/SuccessfulPopup/SuccessfulPopup.js
+++ src/components/SuccessfulPopup/SuccessfulPopup.js
@@ -5,10 +5,10 @@ import './SuccessfulPopup.css';
 function SuccessfulPopup(props) {
   return (
     <div className={`popup ${props.isOpen ? 'popup_active' : ''}`}>
-      <div className="popup__container">
+      <div className="popup__form">
         <button onClick={props.onClose} type="button" className="popup__close-button"></button>
         <h2 className="popup__title-info">Пользователь успешно зарегистрирован!</h2>
-        <button onClick={props.onLinkClick} type="button" className="popup__button-link">Войти</button>
+        <button onClick={props.handleLoginPopupClick} type="button" className="popup__button-link">Войти</button>
       </div>
     </div>
   )
